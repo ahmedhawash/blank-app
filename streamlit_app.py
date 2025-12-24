4b3e2cdc86777633295d8b96dde04d0cb4053bd0
ahmedhawash <30882305+ahmedhawash@users.noreply.github.com>
2025-12-24 11:34:05 +0000

    Multipage try

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+get_started_page = st.Page("get_started", title="Get Started", icon=":material/add_circle:")
+quality_tracker_page = st.Page("quality_tracker.py", title="Quality Tracker", icon=":material/delete:")
+
+pg = st.navigation([get_started_page, quality_tracker_page])
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+pg.run()
